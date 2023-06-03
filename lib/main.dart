a745ab92c07656af8cfb6c2be573993ded634453
Md Didarul islam <islamdidarmd@gmail.com>
2023-06-03 17:18:16 +0600

    Init

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:personal_portfolio_web_app/portfolio_app.dart';
+
+void main() {
+  runApp(const PortfolioApp());
+}
---
+++ lib/portfolio_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
